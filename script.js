e73a7ba575b6391b7f616335f3f580350d061363
mariabillig <mariaebilligdev@gmail.com>
2026-03-02 18:10:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ const menu = document.getElementById("menu");
 
 toggle.addEventListener("click", () => {
     menu.classList.toggle("active");
+
+});
+
+const backToTop = document.querySelector(".back-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 400) {
+    backToTop.style.opacity = "1";
+    backToTop.style.visibility = "visible";
+  } else {
+    backToTop.style.opacity = "0";
+    backToTop.style.visibility = "hidden";
+  }
 });
